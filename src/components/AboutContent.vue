be580a3db20058bd0d7ff567b5fb1109ebcb5957
zzt <zzt@gzsunrun.cn>
2024-03-19 17:20:24 +0800

    init

---
+++ src/components/AboutContent.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter'
+const counter = useCounterStore()
+</script>
+<template>
+  <div class="about-content">
+    <h1>This is an 关于 内容</h1>
+    <div>
+      <a-statistic title="Active Users" :value="counter.doubleCount"  />
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about-content {
+    padding-top: 50px;
+  }
+}
+</style>
