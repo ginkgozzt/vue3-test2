975312d2230a07499b04a15454ddcf4913243459
zzt <zzt@gzsunrun.cn>
2024-03-19 17:32:44 +0800

    feat:插件安装

---
+++ src/components/FormComponents/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter'
+const counter = useCounterStore()
+</script>
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div>
+      <span>计数:</span>
+      <span>{{ counter.count }}</span>
+      <a-button type="primary" @click="counter.increment">增加计算</a-button>
+      <a-button type="primary" @click="counter.decrement">减少计算</a-button>
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    margin: auto;
+    padding-top: 100px;
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import Antd from 'ant-design-vue';
+import ViewUIPlus from 'view-ui-plus'
 import 'ant-design-vue/dist/reset.css';
-
+import 'view-ui-plus/dist/styles/viewuiplus.css'
 const app = createApp(App)
-
 app.use(createPinia())
 app.use(router)
 app.use(Antd)
+app.use(ViewUIPlus)
 
 app.mount('#app')
